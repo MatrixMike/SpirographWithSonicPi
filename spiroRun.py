6e826fd2e6fcf02d255f7bd7585cb558ada15123
Robin Newman <robin.newman@gmail.com>
2018-12-17 09:45:58 +0000

    Update spiroRun.py adding support for randcol argument

--- spiroRun.py
+++ spiroRun.py
@@ -1,4 +1,5 @@
 #spiroRun.py by Robin Newman, December 2018
+#version 2 adding support for randcol argument
 from Spirograph import Spirograph
 import argparse
 import time
@@ -7,16 +8,18 @@ parser.add_argument("-r")
 parser.add_argument("-sr")
 parser.add_argument("-d")
 parser.add_argument("-col")
+parser.add_argument("-rand")
 args=parser.parse_args()
 r=int(args.r)
 sr=int(args.sr)
 d=int(args.d)
 col=args.col
+randcol=args.rand
 
 s = Spirograph(r)
 s.setSmallCircle(sr)
 s.setPen(d, col)
 
-s.draw()
+s.draw(randcol)
 
 time.sleep(2)
